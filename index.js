26c0da226a6a0a398a6ac81ab8f43e754c7c8482
Kristin Kraemer <kristinhannahk@gmail.com>
2019-11-20 23:01:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-function wakeDog(dogName, dogBreed){
+const wakeDog = function(dogName, dogBreed){
   console.log(`Wake ${dogName} the ${dogBreed}`);
   return `Wake ${dogName} the ${dogBreed}`
 }
 
-function leashDog(dogName, dogBreed){
+const leashDog = function(dogName, dogBreed){
   console.log(`Leash ${dogName} the ${dogBreed}`);
   return `Leash ${dogName} the ${dogBreed}`
 }
 
-function walkToPark(dogName, dogBreed){
+const walkToPark = function(dogName, dogBreed){
   console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
   return `Walk to the park with ${dogName} the ${dogBreed}`
 }
 
-const throwFrisbee function (dogName, dogBreed){
+const throwFrisbee = function (dogName, dogBreed){
   console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
   return `Throw the frisbee for ${dogName} the ${dogBreed}`
 }
 
-function walkHome(dogName, dogBreed){
+const walkHome = function(dogName, dogBreed){
   console.log(`Walk home with ${dogName} the ${dogBreed}`);
   return `Walk home with ${dogName} the ${dogBreed}`
 }
 
-function unleashDog(dogName, dogBreed){
+const unleashDog = function(dogName, dogBreed){
   console.log(`Unleash ${dogName} the ${dogBreed}`);
   return `Unleash ${dogName} the ${dogBreed}`
 }
