7f28689ed6ca64fb0a89bbbe0a2f22ffa61a1477
Kristin Kraemer <kristinhannahk@gmail.com>
2019-11-20 22:44:36 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,29 @@
+function wakeDog(dogName, dogBreed){
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+  return `Wake ${dogName} the ${dogBreed}`
+}
+
+function leashDog(dogName, dogBreed){
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+  return `Wake ${dogName} the ${dogBreed}`
+}
+
+function walkToPark(dogName, dogBreed){
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+  return `Wake ${dogName} the ${dogBreed}`
+}
+
+function throwFrisbee(dogName, dogBreed){
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+  return `Wake ${dogName} the ${dogBreed}`
+}
+
+function walkHome(dogName, dogBreed){
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+  return `Wake ${dogName} the ${dogBreed}`
+}
+
+function unleashDog(dogName, dogBreed){
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+  return `Wake ${dogName} the ${dogBreed}`
+}
